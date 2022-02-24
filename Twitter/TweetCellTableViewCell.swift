7e48351d16aca08f733abadd14d408f020b97c54
IkennaAku <ikennaokenwa2004@gmail.com>
2022-02-23 19:03:16 -0600

    Load tweets

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Center for Innovation on 2/23/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
